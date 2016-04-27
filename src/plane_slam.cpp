695ab260eb530043c83c25222ca30f6eadc3b97c
hicopyht <lizhi1205@gmail.com>
2016-04-27 14:17:55 +0800

    v1

--- src/plane_slam.cpp
+++ src/plane_slam.cpp
@@ -1,74 +1,70 @@
 #include "plane_slam.h"
 
-PlaneSlam::PlaneSlam()
+PlaneSlam::PlaneSlam() :
+    isam2_parameters_()
+  , graph_()
+  , initial_estimate_()
+  , first_pose_(true)
+  , odom_poses_()
+  , pose_count_(0)
+  , landmakr_count_(0)
 {
-
+    isam2_parameters_.relinearizeThreshold = 0.05;
+    isam2_parameters_.relinearizeSkip = 1;
+    isam2_parameters_.print("ISAM2 parameters:");
+    isam2_ = new ISAM2(isam2_parameters_);
 }
 
-void PlaneSlam::planeSlam()
+void PlaneSlam::initialize(const Pose3 &pose, const std::vector<Eigen::VectorXd> &plane_coeffs,
+                           const std::vector<Eigen::MatrixXd> &covariances)
 {
-
-    ISAM2Params parameters;
-    parameters.relinearizeThreshold = 0.05;
-    parameters.relinearizeSkip = 1;
-    parameters.print("ISAM2 parameters:");
-    ISAM2 isam(parameters);
-
-    // Create a Factor Graph and Values to hold the new data
-    NonlinearFactorGraph graph; // factor graph
-    Values initialEstimate; // initial guess
-
-
-    // Add factors for each landmark observation
-//    graph.add();
-
-    // Add odometry factor
-
+    // Add a prior factor
+    pose_count_ = 0;
+    noiseModel::Diagonal::shared_ptr poseNoise = noiseModel::Diagonal::Sigmas((Vector(6) << Vector3::Constant(0.3),Vector3::Constant(0.1))); // 30cm std on x,y,z 0.1 rad on roll,pitch,yaw
+    graph_.push_back(PriorFactor<Pose3>(Symbol('x', 0), poses[0], poseNoise));
     // Add an initial guess for the current pose
+    initial_estimate_.insert(Symbol('x', 0), pose);
+    odom_poses_.push_back(pose);
+    pose_count_++;
 
-    // If first pose, add a pose prior
-
-    // If first observed landmark, add a landmark prior
-
-    // do incrementally update, if not first pose
-
-
-    /*
-    // If this is the first iteration, add a prior on the first pose to set the coordinate frame
-    // and a prior on the first landmark to set the scale
-    // Also, as iSAM solves incrementally, we must wait until each is observed at least twice before
-    // adding it to iSAM.
-    if( i == 0)
+    // Add new observation
+    landmakr_count_ = 0;
+    for(int i = 0; i < planes.size(); i++)
     {
-      // Add a prior on pose x0
-      noiseModel::Diagonal::shared_ptr poseNoise = noiseModel::Diagonal::Sigmas((Vector(6) << Vector3::Constant(0.3),Vector3::Constant(0.1))); // 30cm std on x,y,z 0.1 rad on roll,pitch,yaw
-      graph.push_back(PriorFactor<Pose3>(Symbol('x', 0), poses[0], poseNoise));
-
-      // Add a prior on landmark l0
-      noiseModel::Isotropic::shared_ptr pointNoise = noiseModel::Isotropic::Sigma(3, 0.1);
-      graph.push_back(PriorFactor<Point3>(Symbol('l', 0), points[0], pointNoise)); // add directly to graph
-
-      // Add initial guesses to all observed landmarks
-      // Intentionally initialize the variables off from the ground truth
-      for (size_t j = 0; j < points.size(); ++j)
-        initialEstimate.insert(Symbol('l', j), points[j].compose(Point3(-0.25, 0.20, 0.15)));
-
-    } else {
-      // Update iSAM with the new factors
-      isam.update(graph, initialEstimate);
-      // Each call to iSAM2 update(*) performs one iteration of the iterative nonlinear solver.
-      // If accuracy is desired at the expense of time, update(*) can be called additional times
-      // to perform multiple optimizer iterations every step.
-      isam.update();
-      Values currentEstimate = isam.calculateEstimate();
-      cout << "****************************************************" << endl;
-      cout << "Frame " << i << ": " << endl;
-      currentEstimate.print("Current estimate: ");
-
-      // Clear the factor graph and values for the next iteration
-      graph.resize(0);
-      initialEstimate.clear();
+        noiseModel::Gaussian::shared_ptr obs_noise = noiseModel::Gaussian::Covariance(covariances[i]);
+        graph_.push_back(OrientedPlane3Factor(plane_coeffs[i], obs_noise, Symbol('x', 0), Symbol('l', i)));
+        landmakr_count_ ++;
     }
-    */
+    //
+    first_pose_ = false;
+}
+
+void PlaneSlam::planeSlam(const Pose3 &odom, const std::vector<Eigen::VectorXd> &plane_coeffs,
+                          const std::vector<Eigen::MatrixXd> &covariances)
+{
+    if(first_pose_)
+    {
+        cout << "You should initialize the map before doing slam." << endl;
+        return;
+    }
+
+    // Add odometry factors
+    noiseModel::Diagonal::shared_ptr odometry_noise =
+            noiseModel::Diagonal::Sigmas((Vector(6) << Vector3(odom.translation().vector() * 0.2), Vector3(odom.rotation().rpy() * 0.1)));
+    graph_.push_back(BetweenFactor<Pose3>(Symbol('x', pose_count_-1), Symbol('x', pose_count_), odom, odometry_noise));
+    // Add pose guess
+    Pose3 new_pose = *odom_poses_.back();
+    new_pose.compose( odom );
+    odom_poses_.push_back( new_pose );
+    initial_estimate_.insert(Symbol('x', pose_count_), new_pose);
+    pose_count_ ++;
+
+    // Match local landmark with map landmarks
+
+
+    // Add factor to exist landmark
+
+    // Add new landmark
+
 
 }
--- src/plane_slam.h
+++ src/plane_slam.h
@@ -2,16 +2,17 @@
 #define PLANE_SLAM_H
 
 #include <gtsam/geometry/Pose3.h>
+#include <gtsam/geometry/OrientedPlane3.h>
 #include <gtsam/inference/Key.h>
 #include <gtsam/inference/Symbol.h>
 #include <gtsam/slam/PriorFactor.h>
 #include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/slam/OrientedPlane3Factor.h>
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
 #include <gtsam/nonlinear/GaussNewtonOptimizer.h>
 #include <gtsam/nonlinear/Marginals.h>
 #include <gtsam/nonlinear/Values.h>
 #include <gtsam/nonlinear/ISAM2.h>
-#include "PlaneFactor.h"
 
 using namespace std;
 using namespace gtsam;
@@ -21,7 +22,25 @@ class PlaneSlam
 public:
     PlaneSlam();
 
-    void planeSlam();
+    void initialize(const Pose3 &pose, const std::vector<Eigen::VectorXd> &plane_coeffs,
+                    const std::vector<Eigen::MatrixXd> &covariances);
+
+    void planeSlam(const Pose3 &odom, const std::vector<Eigen::VectorXd> &plane_coeffs,
+                   const std::vector<Eigen::MatrixXd> &covariances);
+
+private:
+
+    ISAM2Params isam2_parameters_;
+    ISAM2* isam2_;
+
+    // Create a Factor Graph and Values to hold the new data
+    NonlinearFactorGraph graph_; // factor graph
+    Values initial_estimate_; // initial guess
+
+    bool first_pose_;
+    vector<Pose3> odom_poses_;
+    int pose_count_;
+    int landmakr_count_;
 };
 
 #endif // PLANE_SLAM_H
